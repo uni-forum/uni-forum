5acfc94c67616e7a373c209f811e73a647e732d3
Ashley Hunter <ashley.hunter@hotmail.co.uk>
2020-02-15 22:49:13 +0000

    Adding test import

--- apps/uni-forum/src/app/app.component.spec.ts
+++ apps/uni-forum/src/app/app.component.spec.ts
@@ -1,13 +1,15 @@
 import { async, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { DialogModule } from '@angular/cdk-experimental/dialog';
+import { SharedModule } from './shared/shared.module';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [AppComponent],
       imports: [
-        DialogModule
+        DialogModule,
+        SharedModule
       ]
     }).compileComponents();
   }));
