9f5d2879ded45ff5b158e4124f8d7de583968581
Ashley Hunter <ashley.hunter@hotmail.co.uk>
2020-02-16 13:56:33 +0000

    Adding discussion component

---
+++ apps/uni-forum/src/app/discussions/components/discussion/discussion.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'uni-discussion',
+  templateUrl: './discussion.component.html',
+  styleUrls: ['./discussion.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class DiscussionComponent {
+
+}
--- apps/uni-forum/src/app/discussions/discussions.module.ts
+++ apps/uni-forum/src/app/discussions/discussions.module.ts
@@ -4,10 +4,11 @@ import { CommonModule } from '@angular/common';
 import { DiscussionsRoutingModule } from './discussions-routing.module';
 import { DiscussionsComponent } from './pages/discussions/discussions.component';
 import { SharedModule } from '../shared/shared.module';
+import { DiscussionComponent } from './components/discussion/discussion.component';
 
 
 @NgModule({
-  declarations: [DiscussionsComponent],
+  declarations: [DiscussionsComponent, DiscussionComponent],
   imports: [
     CommonModule,
     DiscussionsRoutingModule,
